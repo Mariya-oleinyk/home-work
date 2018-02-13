a1a201146c7cf6bc34b5a2f4b7db829033e83c5f
Mariya-oleinyk <30420893+Mariya-oleinyk@users.noreply.github.com>
2018-02-13 23:23:59 +0300

    Update scripts.js

--- Module-8/js/scripts.js
+++ Module-8/js/scripts.js
@@ -1,4 +1,3 @@
-
  const playSound = note => {
   const audio = document.querySelector(`audio[data-note=${note}]`);
   audio.currentTime = 0;
@@ -8,39 +7,16 @@
 const buttons = Array.from(document.querySelectorAll("button"));
 const keys = "qwertyuiop[]asdfghjkl;'zxcvbnm,./".split("");
 
-window.addEventListener('keydown', function(e){
-    if(keys.includes(e.key)){
-     let currentButton = buttons.find((a) => a.innerHTML === e.key || a.innerHTML === "space" )
-         currentButton.classList.add('keyboard__btn--active');
-         setTimeout(()=>currentButton.classList.remove('keyboard__btn--active'), 100);
-         let soundCheckbox = document.getElementById('slideThree');
-         if(soundCheckbox.checked){
+  window.addEventListener("keydown", function callback (e){
+        if(keys.includes(e.key)){
+            let currentButton = buttons.find((a) => a.innerHTML === e.key || a.innerHTML === "space" );
+            currentButton.classList.add('keyboard__btn--active');
+            setTimeout(()=>currentButton.classList.remove('keyboard__btn--active'), 100);
+            let soundCheckbox = document.getElementById('slideThree');
+         
+      if(soundCheckbox.checked){
             playSound(currentButton.dataset.note);
          }
-});
+        }
+} )
 
-
-
-
-
-//   window.addEventListener("keydown", function callback (e){
-//         if(keys.includes(e.key)){
-//             let currentButton = buttons.find((a) => a.innerHTML === e.key || a.innerHTML === "space" );
-//             const note = currentButton.getAttribute("data-note");
-//             playSound(note);
-//             currentButton.classList.add("keyboard__btn--active");
-//         }
-// } )
-
-
-
-
-// window.addEventListener("click", function check(input){
-//     input = document.getElementById("slideThree");
-//     let currentButton = callback(e);
-//     if(input.checked === true) {
-//         playSound(currentButton.getAttribute("data-note"));
-//     } else if (input.checked == false){
-//         playSound(currentButton.removeAttribute("data-note"));
-//     }
-// });
