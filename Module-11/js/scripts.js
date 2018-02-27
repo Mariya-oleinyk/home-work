393774df170ed4b8020b9894af0020758a16caa0
Mariya-oleinyk <30420893+Mariya-oleinyk@users.noreply.github.com>
2018-02-28 00:23:18 +0300

    Update scripts.js

--- Module-11/js/scripts.js
+++ Module-11/js/scripts.js
@@ -1,21 +1,16 @@
-const wrapper = document.querySelector(".wrapper");
-const list = document.querySelector(".list");
-const block = document.querySelector(".block");
-const compiled = _.template(list);
-
 function getUsers (){
-    fetch(`http://fecore.net.ua/rest/`)
+    let wrapper = document.querySelector(".wrapper");
+    let listName = document.querySelector(".list_name");
+fetch(`http://fecore.net.ua/rest/`)
    .then(res => res.json())
    .then(res => {
-    let test = "";
-     res.forEach(element => {
-        test += compiled(element); 
-     });
-     block.innerHTML = test; 
-    });
-};
+     console.log(res);
+     for(let i = 0; i < res.length; i++){
+       wrapper.innerHTML += `<h3> ID: ${res[i].name}<h4>`;
+       wrapper.innerHTML += `<h4>Name: ${res[i].name}<h4>`;
+       wrapper.innerHTML += `<p>Score: ${res[i].score}</p>`;
+     }
+   }); 
+}
+
 getUsers ();
-
-// function addUser (){
-
-// }
